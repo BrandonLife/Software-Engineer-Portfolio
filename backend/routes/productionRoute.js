fdaf76a78ed746e7a66fceda4efeea43a0c13104
Brandon Life <Blife1119@gmail.com>
2024-05-10 03:30:40 -0400

    updated ProductionRoute

--- backend/routes/productionRoute.js
+++ backend/routes/productionRoute.js
@@ -3,10 +3,16 @@ const {
 	getProjects,
 	saveProject,
 } = require("../controllers//productionControllers");
+const {
+    sendEmail
+} = require("../controllers/EmailControllers")
+
+
+
 
 router.get("/get", getProjects);
 
 router.post("/save", saveProject);
-
+router.post('/email/send', sendEmail)
 
 module.exports = router;
